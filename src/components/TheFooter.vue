75421d0f1744262cb0a8708d0ae92596fca0f725
SalvatoreBono <salvatore.bono2001@libero.it>
2023-08-22 16:08:09 +0200

    header complete and footer 50%

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,13 +1,132 @@
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <template>
-
+  <div class="bg-dc">
+    <div class="container text-white">
+      <div class="d-flex justify-content-between">
+        <div class="d-flex pt-5 pb-4">
+          <div class="pe-5">
+            <h4>DC COMICS</h4>
+            <ul class="me-auto mb-2 mb-lg-0 list-unstyled text-white">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Characters</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Comics</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Movies</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">TV</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Games</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Videos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">News</a>
+              </li>
+            </ul>
+            <div class="pe-0 pt-4 pb-5">
+              <h4>SHOP</h4>
+              <ul class="me-auto mb-2 mb-lg-0 list-unstyled text-white">
+                <li class="nav-item">
+                  <a class="nav-link" aria-current="page" href="#">Shop DC</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Shop DC Collectibles</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="pe-5">
+            <h4>DC</h4>
+            <ul class="me-auto mb-2 mb-lg-0 list-unstyled text-white">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#"
+                  >Terms of Use</a
+                >
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Privacy policy (New)</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Ad Choices</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Advertising</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Jobs</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Subcriptions</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Talent WorkShops</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">CPSC Certificates</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Ratings</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Shop Help</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h4>SITES</h4>
+            <ul class="me-auto mb-2 mb-lg-0 list-unstyled text-white">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">DC</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">MAD Magazine</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">DC Kids</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">DC Universe</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">DC Power Visa</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="logo-footer"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped >
-
+<style lang="scss" scoped>
+.bg-dc {
+  background-image: url(../assets/img/footer-bg.jpg);
+  background-repeat: repeat-x;
+}
+h4 {
+  padding-bottom: 1rem;
+}
+li {
+  color: rgb(149, 149, 149);
+}
+.logo-footer {
+  background-image: url(../assets/img/dc-logo-bg.png);
+  width: 600px;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
 </style>
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,13 +1,60 @@
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <template>
-
+  <nav class="navbar bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img
+          src="../assets/img/dc-logo.png"
+          alt="Bootstrap"
+          width="60"
+          height="60"
+        />
+      </a>
+      <div>
+        <ul class="d-flex me-auto mb-2 mb-lg-0 list-unstyled">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">CHARACTERS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">COMICS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">MOVIES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">TV</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">GAMES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">COLLECTIBLES</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">VIDEOS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">FANS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">NEWS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">SHOP</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
-
+li {
+  padding-left: 1rem;
+  padding-right: 1rem;
+  font-weight: 700;
+}
 </style>
