f852be6587750e43df9ec43a4e23ca247b219e9d
SalvatoreBono <salvatore.bono2001@libero.it>
2023-08-23 16:05:33 +0200

    finish ex but not used props

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
 import TheFooter from "./components/TheFooter.vue";
 import TheHeader from "./components/TheHeader.vue";
 import TheMainBottom from "./components/TheMainBottom.vue";
+import TheMainTop from "./components/TheMainTop.vue";
 
 export default {
   components: {
     TheHeader,
     TheMainBottom,
     TheFooter,
+    TheMainTop,
   },
 };
 </script>
 
 <template>
   <TheHeader></TheHeader>
+  <TheMainTop></TheMainTop>
   <TheMainBottom></TheMainBottom>
   <TheFooter></TheFooter>
 </template>
---
+++ src/components/TheMainTop.vue
@@ -0,0 +1,58 @@
+<script>
+import dcComics from "../db/dc-comics.json";
+export default {
+  data() {
+    return {
+      dcComics,
+    };
+  },
+  methods: {},
+  mounted() {},
+};
+</script>
+
+<template>
+  <div class="jumbo"></div>
+  <div class="bg-dark">
+    <div class="container position-relative">
+      <div class="container-series text-white bg-primary p-2">
+        <h3 class="title-series">CURRENT SERIES</h3>
+      </div>
+      <div class="pt-5 text-white pb-3">
+        <div class="row row-cols-6 gy-5">
+          <div v-for="singleComics in dcComics" class="col pb-5">
+            <img :src="`${singleComics.thumb}`" alt="" />
+            <div class="pt-3 text-uppercase">{{ singleComics.series }}</div>
+          </div>
+
+          <button
+            type="button"
+            class="btn btn-primary m-auto mt-5 rounded-0 fw-bold"
+          >
+            LOAD MORE
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.jumbo {
+  background-image: url(../assets/img/jumbotron.jpg);
+  background-size: cover;
+  height: 400px;
+}
+.container-series {
+  position: absolute;
+  top: -30px;
+}
+img {
+  width: 100%;
+  height: 100%;
+  aspect-ratio: 1/1;
+}
+.title-series {
+  font-weight: 600;
+}
+</style>
